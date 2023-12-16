8f333249d0d44d2e98c8ad8e2b0ff0be0b109492
cristianFleita <cristianagustinfleita@gmail.com>
2023-12-16 09:19:24 -0300

    Added grid generation dynamically

---
+++ Assets/PathFinding/Scripts/TileGeneration/GridGenerator.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridGenerator : MonoBehaviour
+{
+    [SerializeField] GameObject tile;
+    [SerializeField] int width;
+    [SerializeField] int length;
+    Vector2Int gridSize;
+
+    private GameObject parent;
+    [SerializeField] Vector3 gridPosition;
+    // Start is called before the first frame update
+    
+    void Start()
+    {
+        gridSize = new Vector2Int(width, length);
+        GenerateGrid();
+    }
+
+    public void GenerateGrid()
+    {
+        TileGenerator tileGenerator;
+        AssignGridParent();
+
+        if (!parent.GetComponent<TileGenerator>())
+            tileGenerator = parent.AddComponent<TileGenerator>();
+        else
+            tileGenerator = parent.GetComponent<TileGenerator>();
+
+        tileGenerator.GenerateGrid(tile, gridSize);
+    }
+
+    void AssignGridParent()
+    {
+        if (parent == null)
+            parent = new GameObject("Grid");
+
+        parent.transform.position = gridPosition;
+    }
+}

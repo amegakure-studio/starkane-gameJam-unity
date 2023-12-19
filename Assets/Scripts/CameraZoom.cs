6981004176307be7bf9b1d7023cbb83bf82df19a
“brenda-mareco” <brendaamareco@gmail.com>
2023-12-19 19:20:42 -0300

    Added limit to the camera zoom

--- Assets/Scripts/CameraZoom.cs
+++ Assets/Scripts/CameraZoom.cs
@@ -8,24 +8,23 @@ public class CameraZoom : MonoBehaviour
 {
     [SerializeField] CinemachineVirtualCamera virtualCamera;
     [SerializeField] float zoomPercentaje = 10f;
-
-    CinemachineComponentBase componentBase;
-    float cameraDistance;
+    private float cameraDistance;
 
     
     // Update is called once per frame
     void Update()
     {
-        if(componentBase == null)
-        {
-            componentBase = virtualCamera.GetCinemachineComponent(CinemachineCore.Stage.Body);   
-        }
         float mouseValue = Input.GetAxis("Mouse ScrollWheel");
         
-        if( mouseValue != 0)
+        if(mouseValue != 0)
         {
             cameraDistance = mouseValue * zoomPercentaje;
-            virtualCamera.m_Lens.FieldOfView -= cameraDistance;
+            float actualZoomValue = virtualCamera.m_Lens.FieldOfView - cameraDistance;
+
+            if(actualZoomValue >= 10 && actualZoomValue <= 100 )
+            {
+                virtualCamera.m_Lens.FieldOfView = actualZoomValue;
+            }
         }
     }
 }

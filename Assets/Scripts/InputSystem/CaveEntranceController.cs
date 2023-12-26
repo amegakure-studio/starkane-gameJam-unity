4b8e7cdecad8e140f22e26dea24d9d9679990abc
cristianFleita <cristianagustinfleita@gmail.com>
2023-12-26 00:14:48 -0300

    Set-up dojo entities in cave scene.
    Co-authored-by: Brenda Mareco <brendaamareco@gmail.com>

--- Assets/Scripts/InputSystem/CaveEntranceController.cs
+++ Assets/Scripts/InputSystem/CaveEntranceController.cs
@@ -5,6 +5,22 @@ using UnityEngine.SceneManagement;
 
 public class CaveEntranceController : MonoBehaviour
 {
+    [SerializeField] GameObject vfxSelectPrefab;
+
+    private void OnMouseEnter()
+    {
+        if(vfxSelectPrefab != null)
+            vfxSelectPrefab.SetActive(true);
+            
+    }
+
+    private void OnMouseExit()
+    {
+        if(vfxSelectPrefab != null)
+            vfxSelectPrefab.SetActive(false);
+       
+    }
+
     private void OnMouseDown()
     {
         SceneManager.LoadScene("Cave");

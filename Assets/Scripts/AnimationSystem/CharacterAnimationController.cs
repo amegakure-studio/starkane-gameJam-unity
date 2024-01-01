369a86890854c60b93e162ad442b0dcf9353b1f0
“brenda-mareco” <brendaamareco@gmail.com>
2024-01-01 02:15:26 -0300

    Set IsDead bool animator

--- Assets/Scripts/AnimationSystem/CharacterAnimationController.cs
+++ Assets/Scripts/AnimationSystem/CharacterAnimationController.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Amegakure.Starkane.PubSub;
 using Amegakure.Starkane.EntitiesWrapper;
+using System;
 
 namespace Amegakure.Starkane.AnimationSystem
 {
@@ -18,12 +19,31 @@ namespace Amegakure.Starkane.AnimationSystem
         {
             EventManager.Instance.Subscribe(GameEvent.CHARACTER_MOVE_START, HandleCharacterMoveStart);
             EventManager.Instance.Subscribe(GameEvent.CHARACTER_MOVE_END, HandleCharacterMoveEnd);
+            EventManager.Instance.Subscribe(GameEvent.COMBAT_CHARACTER_DEAD, HandleCharacterDead);
         }
 
         private void OnDisable()
         {
             EventManager.Instance.Unsubscribe(GameEvent.CHARACTER_MOVE_START, HandleCharacterMoveStart);
             EventManager.Instance.Unsubscribe(GameEvent.CHARACTER_MOVE_END, HandleCharacterMoveEnd);
+            EventManager.Instance.Unsubscribe(GameEvent.COMBAT_CHARACTER_DEAD, HandleCharacterDead);
+        }
+
+        private void HandleCharacterDead(Dictionary<string, object> context)
+        {
+            try
+            {
+                Character character = (Character)context["Character"];
+
+
+                if (!characterAnimatorMap.ContainsKey(character))
+                    characterAnimatorMap[character] = character.GetComponent<Animator>();
+
+                Animator animator = characterAnimatorMap[character];
+                animator?.SetBool("IsDead", true);
+
+            }
+            catch { }
         }
 
         private void HandleCharacterMoveStart(Dictionary<string, object> context)
